4b26e94c36e1a54b16c95a40af42175cb7cc099f
Matheus1706-dog <carvalhomatheus080@gmail.com>
2025-05-30 09:12:39 -0300

    Add files via upload

---
+++ saque.h
@@ -0,0 +1,8 @@
+#ifndef SAQUE_H
+#define SAQUE_H
+
+#include "usuario.h"
+
+void realizarSaque(Usuario* u);
+
+#endif
---
+++ usuario.h
@@ -0,0 +1,10 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+typedef struct {
+    char nome[100];
+    char senha[20];
+    float saldo;
+} Usuario;
+
+#endif
